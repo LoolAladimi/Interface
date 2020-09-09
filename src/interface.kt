5a7d66b1509ff8a80c854a5a40cb4768b88a63d7
lool-aladimi <loolaladimi@gmail.com>
2020-09-09 11:14:20 +0300

    Initial commit

---
+++ src/interface.kt
@@ -0,0 +1,21 @@
+interface lool{
+    fun click()
+    fun doubleClick()
+}
+interface lool2:lool{
+    fun show()
+    }
+class lool3:lool2{
+    override fun show() {
+        TODO("Not yet implemented")
+    }
+
+    override fun click() {
+        TODO("Not yet implemented")
+    }
+
+    override fun doubleClick() {
+        TODO("Not yet implemented")
+    }
+
+}
